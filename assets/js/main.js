30769b840fd3405358c3ee806b542acde1a412ab
Jolinnnnnnnn <mahjy-wm20@student.tarc.edu.my>
2023-09-19 22:31:00 +0800

    test N3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,215 +1,244 @@
 /**
-* Template Name: Mentor
-* Updated: Aug 30 2023 with Bootstrap v5.3.1
-* Template URL: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-(function() {
+ * Template Name: Mentor
+ * Updated: Aug 30 2023 with Bootstrap v5.3.1
+ * Template URL: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/
+ * Author: BootstrapMade.com
+ * License: https://bootstrapmade.com/license/
+ */
+(function () {
   "use strict";
 
   /**
    * Easy selector helper function
    */
   const select = (el, all = false) => {
-    el = el.trim()
+    el = el.trim();
     if (all) {
-      return [...document.querySelectorAll(el)]
+      return [...document.querySelectorAll(el)];
     } else {
-      return document.querySelector(el)
+      return document.querySelector(el);
     }
-  }
+  };
 
   /**
    * Easy event listener function
    */
   const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
+    let selectEl = select(el, all);
     if (selectEl) {
       if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
+        selectEl.forEach((e) => e.addEventListener(type, listener));
       } else {
-        selectEl.addEventListener(type, listener)
+        selectEl.addEventListener(type, listener);
       }
     }
-  }
+  };
 
   /**
-   * Easy on scroll event listener 
+   * Easy on scroll event listener
    */
   const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
-  }
+    el.addEventListener("scroll", listener);
+  };
 
   /**
    * Back to top button
    */
-  let backtotop = select('.back-to-top')
+  let backtotop = select(".back-to-top");
   if (backtotop) {
     const toggleBacktotop = () => {
       if (window.scrollY > 100) {
-        backtotop.classList.add('active')
+        backtotop.classList.add("active");
       } else {
-        backtotop.classList.remove('active')
+        backtotop.classList.remove("active");
       }
-    }
-    window.addEventListener('load', toggleBacktotop)
-    onscroll(document, toggleBacktotop)
+    };
+    window.addEventListener("load", toggleBacktotop);
+    onscroll(document, toggleBacktotop);
   }
 
   /**
    * Mobile nav toggle
    */
-  on('click', '.mobile-nav-toggle', function(e) {
-    select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
-  })
+  on("click", ".mobile-nav-toggle", function (e) {
+    select("#navbar").classList.toggle("navbar-mobile");
+    this.classList.toggle("bi-list");
+    this.classList.toggle("bi-x");
+  });
 
   /**
    * Mobile nav dropdowns activate
    */
-  on('click', '.navbar .dropdown > a', function(e) {
-    if (select('#navbar').classList.contains('navbar-mobile')) {
-      e.preventDefault()
-      this.nextElementSibling.classList.toggle('dropdown-active')
-    }
-  }, true)
+  on(
+    "click",
+    ".navbar .dropdown > a",
+    function (e) {
+      if (select("#navbar").classList.contains("navbar-mobile")) {
+        e.preventDefault();
+        this.nextElementSibling.classList.toggle("dropdown-active");
+      }
+    },
+    true
+  );
 
   /**
    * Preloader
    */
-  let preloader = select('#preloader');
+  let preloader = select("#preloader");
   if (preloader) {
-    window.addEventListener('load', () => {
-      preloader.remove()
+    window.addEventListener("load", () => {
+      preloader.remove();
     });
   }
 
   /**
    * Testimonials slider
    */
-  new Swiper('.testimonials-slider', {
+  new Swiper(".testimonials-slider", {
     speed: 600,
     loop: true,
     autoplay: {
       delay: 5000,
-      disableOnInteraction: false
+      disableOnInteraction: false,
     },
-    slidesPerView: 'auto',
+    slidesPerView: "auto",
     pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
     },
     breakpoints: {
       320: {
         slidesPerView: 1,
-        spaceBetween: 20
+        spaceBetween: 20,
       },
 
       1200: {
         slidesPerView: 2,
-        spaceBetween: 20
-      }
-    }
+        spaceBetween: 20,
+      },
+    },
   });
 
   /**
    * Animation on scroll
    */
-  window.addEventListener('load', () => {
+  window.addEventListener("load", () => {
     AOS.init({
       duration: 1000,
-      easing: 'ease-in-out',
+      easing: "ease-in-out",
       once: true,
-      mirror: false
-    })
+      mirror: false,
+    });
   });
 
   /**
-   * Initiate Pure Counter 
+   * Initiate Pure Counter
    */
   new PureCounter();
 
   /**
    * Display respective entry qualification input field
    */
-  let entryQualificationDropdown = select('#entryQualificationDropdown');
-  let triggeredQualificationSTPMAUEC = select('#triggeredQualificationSTPMAUEC');
-  let triggeredQualificationSPM = select('#triggeredQualificationSPM');
-  let triggeredQualificationDip = select('#triggeredQualificationDip');
-  let triggeredQualificationFound = select('#triggeredQualificationFound');
+  let entryQualificationDropdown = select("#entryQualificationDropdown");
+  let triggeredQualificationSTPMAUEC = select(
+    "#triggeredQualificationSTPMAUEC"
+  );
+  let triggeredQualificationSPM = select("#triggeredQualificationSPM");
+  let triggeredQualificationDip = select("#triggeredQualificationDip");
+  let triggeredQualificationFound = select("#triggeredQualificationFound");
 
-  entryQualificationDropdown.addEventListener('change',function(){
-    let selectedEntryQualification=entryQualificationDropdown.value;
+  entryQualificationDropdown.addEventListener("change", function () {
+    let selectedEntryQualification = entryQualificationDropdown.value;
 
-    triggeredQualificationSTPMAUEC.style.display='none';
-    triggeredQualificationSPM.style.display='none';
-    triggeredQualificationDip.style.display='none';
-    triggeredQualificationFound.style.display='none';
+    triggeredQualificationSTPMAUEC.style.display = "none";
+    triggeredQualificationSPM.style.display = "none";
+    triggeredQualificationDip.style.display = "none";
+    triggeredQualificationFound.style.display = "none";
 
-    if(selectedEntryQualification=='stpmAUEC'){
-      triggeredQualificationSTPMAUEC.style.display='block';
+    if (selectedEntryQualification == "stpmAUEC") {
+      triggeredQualificationSTPMAUEC.style.display = "block";
+    } else if (selectedEntryQualification == "spm") {
+      triggeredQualificationSPM.style.display = "block";
+    } else if (selectedEntryQualification == "diploma") {
+      triggeredQualificationDip.style.display = "block";
+    } else if (selectedEntryQualification == "foundation") {
+      triggeredQualificationFound.style.display = "block";
+    } else {
+      triggeredQualificationSTPMAUEC.style.display = "none";
+      triggeredQualificationSPM.style.display = "none";
+      triggeredQualificationDip.style.display = "none";
+      triggeredQualificationFound.style.display = "none";
     }
-    else if(selectedEntryQualification=='spm'){
-      triggeredQualificationSPM.style.display='block';
-    }
-    else if(selectedEntryQualification=='diploma'){
-      triggeredQualificationDip.style.display='block';
-    }
-    else if(selectedEntryQualification=='foundation'){
-      triggeredQualificationFound.style.display='block';
-    }
-    else{
-      triggeredQualificationSTPMAUEC.style.display='none';
-      triggeredQualificationSPM.style.display='none';
-      triggeredQualificationDip.style.display='none';
-      triggeredQualificationFound.style.display='none';
-    }
-  })
+  });
+})();
 
-  /**
-   * auto compare qualification
-   */
-  function validateForm() {
-    var dipCGPA = document.forms["registrationForm"]["dipCGPA"].value;
-    //var password = document.forms["registrationForm"]["password"].value;
+/**
+ * auto compare qualification
+ */
+function validateForm() {
+  let entryRequirementValue = entryQualificationDropdown.value;
 
-    // Add your validation logic here
-    if (dipCGPA < 2.5) {
-        alert("Sorry, you are rejected for the course. Minimum CGPA required is 2.5.");
-        return false; // Prevent form submission
-    }
+  // Foundation variable
+  let foundCGPA = document.forms["registrationForm"]["foundCGPA"].value;
+  
+  // Diploma variable
+  let dipCGPA = document.forms["registrationForm"]["dipCGPA"].value;
 
-    // If validation passes, allow the form to submit
-    return true;
+  // STPM variables
+  let stpmMathMGrade = stpmMathMGrade.value;
+  let stpmMathTGrade = document.getElementById("stpmMathTGrade");
+  let stpmICTGrade = document.getElementById("stpmICTGrade");
+  let stpmPhyGrade = document.getElementById("stpmPhyGrade");
+  let stpmChmGrade = document.getElementById("stpmChmGrade");
+  let stpmBioGrade = document.getElementById("stpmBioGrade");
+
+  // SPM variables
+  let spmBMGrade = document.forms["registrationForm"]["spmBMGrade"].value;
+  let spmEngGrade = document.forms["registrationForm"]["spmEngGrade"].value;
+  let spmSjGrade = document.forms["registrationForm"]["spmSjGrade"].value;
+  let spmAMGrade = document.forms["registrationForm"]["spmAMGrade"].value;
+  let spmMathsGrade = document.forms["registrationForm"]["spmMathsGrade"].value;
+  let spmPhyGrade = document.forms["registrationForm"]["spmPhyGrade"].value;
+  let spmChmGrade = document.forms["registrationForm"]["spmChmGrade"].value;
+  let spmBioGrade = document.forms["registrationForm"]["spmBioGrade"].value;
+  let spmMoralGrade = document.forms["registrationForm"]["spmMoralGrade"].value;
+  alert(stpmMathMGrade);
+  // Default error message
+  // let errorMessage = "Sorry, you are rejected for this course. Minimum CGPA/Grade required is 2.5 (C).";
+  // let hasError = false; // Flag to track if an error occurred
+
+  // if (entryRequirementValue == "foundation" && foundCGPA < 2.5) {
+  //   alert(errorMessage);
+  //   hasError = true;
+  // }
+
+  // if (entryRequirementValue == "diploma" && dipCGPA < 2.5) {
+  //   alert(errorMessage);
+  //   hasError = true;
+  // }
+
+  // if (entryRequirementValue == "stpmAUEC" &&
+  //   (stpmMathMGrade > "C" || stpmMathTGrade > "C" || stpmICTGrade > "C" || stpmPhyGrade > "C" || stpmChmGrade > "C" || stpmBioGrade > "C")) {
+  //   alert(errorMessage);
+  //   hasError = true;
+  // }
+
+  // if (entryRequirementValue == "spm" &&
+  //   (spmBMGrade > "C" || spmEngGrade > "C" || spmSjGrade > "C" || spmAMGrade > "C" || spmMathsGrade > "C" || spmPhyGrade > "C" || spmChmGrade > "C" || spmBioGrade > "C" || spmMoralGrade > "C")) {
+  //   alert(errorMessage);
+  //   hasError = true;
+  // }
+
+  // if (!hasError) {
+  //   alert("You are registered successfully!");
+  // }
+
+  // return !hasError; // If validation passes, allow the form to submit
 }
 
 
 
-//   document.addEventListener("DOMContentLoaded", function () {
-//     let registrationForm = document.getElementById('triggeredQualificationDip'); // Updated ID
-//     let diplomaCGPA = document.getElementById('dipCGPA');
-//     let enrolResult = document.getElementById('enrolResult');
 
-//     registrationForm.addEventListener("submit", function (event) {
-//         event.preventDefault();
 
-//         const cgpa = parseFloat(diplomaCGPA.value);
-//         console.log("CGPA entered:", cgpa); // Add this line to check the value of cgpa
-        
-//         if (isNaN(cgpa)) {
-//             enrolResult.innerText = "Please enter a valid CGPA.";
-//         } 
-//         else {
-//             if (cgpa >= 2.5) {
-//                 enrolResult.innerText = "Congratulations! You are accepted for the course.";
-//             } else {
-//                 enrolResult.innerText = "Sorry, you are rejected for the course. Minimum CGPA required is 2.5.";
-//             }
-//           }
-//     });
-// });
 
-})()
--- enrolment.php
+++ enrolment.php
@@ -1,69 +1,70 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  // 定义要接收的字段列表
-  $fields = ['firstName', 'lastName', 'email', 'genderDropdown', 'birthdate', 'address','postcode','city','state','countryDropdown','programmeDropdown','campusDropdown','entryQualificationDropdown','stpmMathMGrade','stpmMathTGrade','stpmICTGrade','stpmPhyGrade','stpmChmGrade','stpmBioGrade','spmBMGrade','spmEngGrade','spmBCGrade','spmSjGrade','spmMathsGrade','spmAMGrade','spmPhyGrade','spmChmGrade','spmBioGrade','spmMoralGrade','dipCGPA','foundCGPA'];
+//   // 定义要接收的字段列表
+//   $fields = ['firstName', 'lastName', 'email', 'genderDropdown', 'birthdate', 'address','postcode','city','state','countryDropdown','programmeDropdown','campusDropdown','entryQualificationDropdown','stpmMathMGrade','stpmMathTGrade','stpmICTGrade','stpmPhyGrade','stpmChmGrade','stpmBioGrade','spmBMGrade','spmEngGrade','spmBCGrade','spmSjGrade','spmMathsGrade','spmAMGrade','spmPhyGrade','spmChmGrade','spmBioGrade','spmMoralGrade','dipCGPA','foundCGPA'];
 
-  // 初始化一个关联数组来存储表单数据
-  $formData = [];
+//   // 初始化一个关联数组来存储表单数据
+//   $formData = [];
 
-  // 循环遍历字段列表并接收表单数据
-  foreach ($fields as $field) {
-      $formData[$field] = isset($_POST[$field]) ? $_POST[$field] : null;
-  }
+//   // 循环遍历字段列表并接收表单数据
+//   foreach ($fields as $field) {
+//       $formData[$field] = isset($_POST[$field]) ? $_POST[$field] : null;
+//   }
 
-  // 检查是否填写了所有必填字段
-  // if (in_array(null, $formData, true)) {
-  //     echo 'Please fill in all required fields.';
-  //     exit;
-  // }
+//   // 检查是否填写了所有必填字段
+//   // if (in_array(null, $formData, true)) {
+//   //     echo 'Please fill in all required fields.';
+//   //     exit;
+//   // }
 
-  // 最低要求的 CGPA 分数
-  $minimum_cgpa = 2.5;
+//   // 最低要求的 CGPA 分数
+//   $minimum_cgpa = 2.5;
 
-  // 根据字段处理表单数据
-  $firstName = $formData['firstName'];
-  $lastName = $formData['lastName'];
-  $email = $formData['email'];
-  $gender = $formData['genderDropdown'];
-  $birthdate = $formData['birthdate'];
-  $address = $formData['address'];
-  $postcode = $formData['postcode'];
-  $city = $formData['city'];
-  $state = $formData['state'];
-  $country = $formData['countryDropdown'];
-  $programme = $formData['programmeDropdown'];
-  $campus = $formData['campusDropdown'];
-  $entryQualification = $formData['entryQualificationDropdown'];
-  $stpmMathMGrade = $formData['stpmMathMGrade'];
-  $stpmMathTGrade = $formData['stpmMathTGrade'];
-  $stpmICTGrade = $formData['stpmICTGrade'];
-  $stpmPhyGrade = $formData['stpmPhyGrade'];
-  $stpmChmGrade = $formData['stpmChmGrade'];
-  $stpmBioGrade = $formData['stpmBioGrade'];
-  $spmBMGrade = $formData['spmBMGrade'];
-  $spmEngGrade = $formData['spmEngGrade'];
-  $spmBCGrade = $formData['spmBCGrade'];
-  $spmSjGrade = $formData['spmSjGrade'];
-  $spmMathsGrade = $formData['spmMathsGrade'];
-  $spmAMGrade = $formData['spmAMGrade'];
-  $spmPhyGrade = $formData['spmPhyGrade'];
-  $spmChmGrade = $formData['spmChmGrade'];
-  $spmBioGrade = $formData['spmBioGrade'];
-  $spmMoralGrade = $formData['spmMoralGrade'];
-  $dipCGPA = $formData['dipCGPA'];
-  $foundCGPA = $formData['foundCGPA'];
+//   // 根据字段处理表单数据
+//   $firstName = $formData['firstName'];
+//   $lastName = $formData['lastName'];
+//   $email = $formData['email'];
+//   $gender = $formData['genderDropdown'];
+//   $birthdate = $formData['birthdate'];
+//   $address = $formData['address'];
+//   $postcode = $formData['postcode'];
+//   $city = $formData['city'];
+//   $state = $formData['state'];
+//   $country = $formData['countryDropdown'];
+//   $programme = $formData['programmeDropdown'];
+//   $campus = $formData['campusDropdown'];
+//   $entryQualification = $formData['entryQualificationDropdown'];
+//   $stpmMathMGrade = $formData['stpmMathMGrade'];
+//   $stpmMathTGrade = $formData['stpmMathTGrade'];
+//   $stpmICTGrade = $formData['stpmICTGrade'];
+//   $stpmPhyGrade = $formData['stpmPhyGrade'];
+//   $stpmChmGrade = $formData['stpmChmGrade'];
+//   $stpmBioGrade = $formData['stpmBioGrade'];
+//   $spmBMGrade = $formData['spmBMGrade'];
+//   $spmEngGrade = $formData['spmEngGrade'];
+//   $spmBCGrade = $formData['spmBCGrade'];
+//   $spmSjGrade = $formData['spmSjGrade'];
+//   $spmMathsGrade = $formData['spmMathsGrade'];
+//   $spmAMGrade = $formData['spmAMGrade'];
+//   $spmPhyGrade = $formData['spmPhyGrade'];
+//   $spmChmGrade = $formData['spmChmGrade'];
+//   $spmBioGrade = $formData['spmBioGrade'];
+//   $spmMoralGrade = $formData['spmMoralGrade'];
+//   $dipCGPA = $formData['dipCGPA'];
+//   $foundCGPA = $formData['foundCGPA'];
 
-  // 检查学生是否被接受或拒绝
-  if ($dipCGPA >= $minimum_cgpa) {
-      echo "Congratulations, $firstName! You are accepted for the $programme program.";
-  } else {
-      echo "Sorry, $firstName. You are rejected for the $programme program. Minimum CGPA required is $minimum_cgpa.";
-  }
+//   // 检查学生是否被接受或拒绝
+//   if ($dipCGPA >= $minimum_cgpa) {
+//       echo "Congratulations, $firstName! You are accepted for the $programme program.";
+//   } else {
+//       echo "Sorry, $firstName. You are rejected for the $programme program. Minimum CGPA required is $minimum_cgpa.";
+//   }
   
-} else {
+} 
+//else {
   // Handle GET request or direct access to this script
-  echo 'This script is not meant to be accessed directly.';
-}
+//   echo 'This script is not meant to be accessed directly.';
+// }
   // Sanitize user inputs
   // $firstName = filter_var($_POST['firstName'], FILTER_SANITIZE_STRING);
   // $lastName = filter_var($_POST['lastName'], FILTER_SANITIZE_STRING);
